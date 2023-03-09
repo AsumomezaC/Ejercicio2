f82e912be643e66ed8026a6050aac7a63b649f3f
AsumomezaC <cuevassolisadrian@gmail.com>
2023-03-09 15:50:03 -0600

    Menu y modificacion de funciones

--- Concesionaria.cs
+++ Concesionaria.cs
@@ -16,50 +16,45 @@ namespace Ejecicio2{
     public void AgregarAuto(Automovil a){
 
       if(a != null){
-        if(noAutosActual <= limiteAutomoviles){
-          listaAutomovil.Add(a);
-          noAutosActual++;
+        if(noAutosActual != limiteAutomoviles){
+          Automovil? autoEncontrado = listaAutomovil.Find((b) => b.id == a.id);
+          if(autoEncontrado == null){
+            listaAutomovil.Add(a);
+           noAutosActual++;
+          }
         }
       }
     }
 
-    public void MostrarAuto(Automovil a){
-      if(a != null){
-        foreach(Automovil item in listaAutomovil){
+    public void MostrarAuto(string id){
 
-          if(item == a){
-            Console.WriteLine(item);
-            return; //evita imprimir el mensaje que no se encontro el auto
-          }
+      if(id != null){
+        Automovil? autoEncontrado = listaAutomovil.Find((a)=> a.id==id);
+        if(autoEncontrado != null){
+          Console.WriteLine(autoEncontrado.ToString());
+          return;
         }
+      }
 
-        // Si no se encontro el auto 
+      // Si no se encontro el auto 
         Console.WriteLine("El automovil buscado no se encuentra en la concessionaria");
       }
-    }
 
-    public void EliminarAuto(Automovil a){
+      public void EliminarAuto(string id){
 
-      if(a != null){
-
-        foreach(Automovil item in listaAutomovil){
-
-          if(item == a){
-            listaAutomovil.Remove(item);
-            noAutosActual--;
-            return; //evita imprimir el mensaje que no se encontro el auto
-          }
+      if(id != "" && noAutosActual != 0){
+        Automovil? autoEcontrado = listaAutomovil.Find((a)=> a.id == id);
+        if(autoEcontrado != null){
+          listaAutomovil.Remove(autoEcontrado);
+          noAutosActual--;
         }
-
-          // Si no se encontro el auto 
-        Console.WriteLine("El automovil no se encuentra en la concessionaria");
       }
     }
 
     public void MostrarAutos(){
 
       foreach(Automovil item in listaAutomovil){
-          Console.WriteLine(item);
+          Console.WriteLine(item.ToString());
 	    }     
     }
     public void VaciarConcesionaria(){
